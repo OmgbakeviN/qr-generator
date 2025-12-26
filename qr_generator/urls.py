1657f957408636fff0be729c0518c306919f2811
OmgbaKevin <kevinomgba1@gmail.com>
2025-12-26 10:40:59 +0100

    adjusted media serving

--- qr_generator/urls.py
+++ qr_generator/urls.py
@@ -1,34 +1,21 @@
-"""
-URL configuration for qr_generator project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import os
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
-from django.contrib.sitemaps.views import sitemap
-from qr.sitemaps import StaticViewSitemap
-
-sitemaps = {"static": StaticViewSitemap()}
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",include("qr.urls")),
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path("admin/", admin.site.urls),
+    path("", include("qr.urls")),
 ]
 
+# Local dev
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Render / production demo (serve MEDIA through Django)
+if os.environ.get("SERVE_MEDIA", "0") == "1":
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
